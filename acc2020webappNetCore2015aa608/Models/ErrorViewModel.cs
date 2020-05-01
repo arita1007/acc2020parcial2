d65aadacfc3a15458c7ebc0ec8fb64e6bea3313f
arita1007 <arita1007@hotmail.com>
2020-05-01 01:22:33 -0600

    Agregar archivos de proyecto.

---
+++ acc2020webappNetCore2015aa608/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace acc2020webappNetCore2015aa608.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
